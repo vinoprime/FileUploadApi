307ec389932d95771e80b9b3b73c961bbb292d45
Vinoth Ragavan <vinothprofessional@outlook.com>
2019-03-21 08:34:31 +0530

    Initial commit

---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask,abort,render_template,request,redirect,url_for
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = './uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        fileName = secure_filename(f.filename)
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], fileName))
+        return 'file uploaded successfully'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
